1ca28e9bd8093425255e735e9aef0f7918d1df28
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-06 14:22:17 -0400

    add navbar & simple routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,28 @@
-import logo from "assets/logo.svg";
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>Boerigter Center App</Navbar.Brand>
+          <Nav>
+            <Link to="/career" className="nav-link">
+              Career Opportunities
+            </Link>
+            <Link to="/alumni" className="nav-link">
+              Find Alumni
+            </Link>
+            <Link to="/study-abroad" className="nav-link">
+              Off-Campus Study
+            </Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </Router>
   );
 }
