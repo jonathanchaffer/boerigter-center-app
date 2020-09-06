b5449350931098814ee981176d00dff4bbde8360
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-06 14:27:01 -0400

    move navbar to its own component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,11 @@
+import { Navigation } from "components";
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export function App(): JSX.Element {
   return (
     <Router>
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand>Boerigter Center App</Navbar.Brand>
-          <Nav>
-            <Link to="/career" className="nav-link">
-              Career Opportunities
-            </Link>
-            <Link to="/alumni" className="nav-link">
-              Find Alumni
-            </Link>
-            <Link to="/study-abroad" className="nav-link">
-              Off-Campus Study
-            </Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Navigation />
     </Router>
   );
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+export function Navigation(): JSX.Element {
+  return (
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Brand>Boerigter Center App</Navbar.Brand>
+        <Nav>
+          <Link to="/career" className="nav-link">
+            Career Opportunities
+          </Link>
+          <Link to="/alumni" className="nav-link">
+            Find Alumni
+          </Link>
+          <Link to="/study-abroad" className="nav-link">
+            Off-Campus Study
+          </Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
