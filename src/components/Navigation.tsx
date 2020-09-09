dc32271aa7a61b7e2205a97db2003ec3395a53c3
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-09 09:49:52 -0400

    apply "active" class to current tab

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function Navigation(): JSX.Element {
   return (
@@ -12,17 +12,25 @@ export function Navigation(): JSX.Element {
           <Navbar.Brand>Boerigter Center App</Navbar.Brand>
         </Link>
         <Nav>
-          <Link to="/career" className="nav-link">
-            Career Opportunities
-          </Link>
-          <Link to="/alumni" className="nav-link">
-            Find Alumni
-          </Link>
-          <Link to="/study-abroad" className="nav-link">
-            Off-Campus Study
-          </Link>
+          <NavLink path="/career">Career Opportunities</NavLink>
+          <NavLink path="/alumni">Find Alumni</NavLink>
+          <NavLink path="/study-abroad">Off-Campus Study</NavLink>
         </Nav>
       </Container>
     </Navbar>
+  );
+}
+
+type NavLinkProps = {
+  path: string;
+  children: React.ReactNode;
+};
+
+function NavLink({ path, children }: NavLinkProps): JSX.Element {
+  const { pathname } = useLocation();
+  return (
+    <Link to={path} className={`nav-link ${pathname === path ? "active" : ""}`}>
+      {children}
+    </Link>
   );
 }
