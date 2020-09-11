042ede7142c133031cbcb3387042c36f363cc167
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-11 11:54:37 -0400

    initialize firebase project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import { App } from "components";
+import firebase from "firebase/app";
 import React from "react";
 import ReactDOM from "react-dom";
 import "styles/fonts.scss";
 import "styles/index.scss";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhvXuBAQBCLHxhM-2QWnYRxDlZ4oZhDhg",
+  appId: "1:600507081671:web:acf828e427865ecb0c2fe2",
+  authDomain: "boerigter-center-app.firebaseapp.com",
+  databaseURL: "https://boerigter-center-app.firebaseio.com",
+  messagingSenderId: "600507081671",
+  projectId: "boerigter-center-app",
+  storageBucket: "boerigter-center-app.appspot.com",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
