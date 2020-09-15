28c3e189d14b3262255a2f4f8c89215ccd2b6f5e
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-15 11:50:42 -0400

    display google map on MapView

--- src/components/MapView.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function MapView(): JSX.Element {
-  return (
-    <div className="d-flex h-100 align-items-center justify-content-center">
-      <span>MapView</span>
-    </div>
-  );
-}
