155c3453ba47652db6edafff496e935de0558923
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-22 12:09:11 -0400

    display clusters on map

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -1,7 +1,9 @@
 import GoogleMapReact from "google-map-react";
 import { Alum } from "models";
-import React from "react";
+import React, { useState } from "react";
 import { parseAlumni } from "services";
+import { ClusterProperties, PointFeature } from "supercluster";
+import useSupercluster from "use-supercluster";
 import "./MapView.scss";
 
 interface MapViewProps {
@@ -9,6 +11,24 @@ interface MapViewProps {
 }
 
 export function MapView({ mapType }: MapViewProps): JSX.Element {
+  const [mapZoom, setMapZoom] = useState(4);
+  const [mapBounds, setMapBounds] = useState<[number, number, number, number]>([0, 0, 0, 0]);
+
+  const points: PointFeature<Alum>[] = parseAlumni().map((alum: Alum) => {
+    return {
+      geometry: { coordinates: [alum.longitude, alum.latitude], type: "Point" },
+      properties: alum,
+      type: "Feature",
+    };
+  });
+
+  const { clusters } = useSupercluster({
+    bounds: mapBounds,
+    options: { maxZoom: 20, radius: 75 },
+    points,
+    zoom: mapZoom,
+  });
+
   return (
     <div className="map-container">
       <GoogleMapReact
@@ -17,16 +37,41 @@ export function MapView({ mapType }: MapViewProps): JSX.Element {
           lat: 39.381266,
           lng: -97.922211,
         }}
-        defaultZoom={4}
+        zoom={mapZoom}
         options={{ maxZoom: 6 }}
+        onChange={({ zoom, bounds }) => {
+          setMapZoom(zoom);
+          setMapBounds([bounds.nw.lng, bounds.se.lat, bounds.se.lng, bounds.nw.lat]);
+        }}
       >
-        {mapType === "alumni" &&
-          parseAlumni().map(alum => {
-            return <AlumPin alum={alum} key={alum.id} lat={alum.latitude} lng={alum.longitude} />;
-          })}
+        {clusters.map(pointOrCluster => {
+          const [longitude, latitude] = pointOrCluster.geometry.coordinates;
+
+          const cluster = pointOrCluster as PointFeature<ClusterProperties>;
+          const { cluster: isCluster, point_count: pointCount } = cluster.properties;
+
+          const point = pointOrCluster as PointFeature<Alum>;
+          const alum = point.properties;
+
+          return isCluster ? (
+            <ClusterPin count={pointCount} key={cluster.id} lat={latitude} lng={longitude} />
+          ) : (
+            <AlumPin alum={alum} key={alum.id} lat={latitude} lng={longitude} />
+          );
+        })}
       </GoogleMapReact>
     </div>
   );
+}
+
+interface ClusterPinProps {
+  count: number;
+  lat: number;
+  lng: number;
+}
+
+function ClusterPin({ count }: ClusterPinProps): JSX.Element {
+  return <span>{count}</span>;
 }
 
 interface AlumPinProps {
