f5feb99d34273d8504b36af8066e2a038bde736c
jonathanchaffer <jonathanchaffer@gmail.com>
2020-10-23 11:35:15 -0400

    make links open in new tab

--- src/components/reusables/AlumContactInfo.tsx
+++ src/components/reusables/AlumContactInfo.tsx
@@ -11,19 +11,19 @@ export function AlumContactInfo({ alum }: AlumContactInfoProps): JSX.Element {
   return (
     <div className="secondary-info d-flex flex-column">
       {alum.website && (
-        <a href={alum.website}>
+        <a href={alum.website} target="blank">
           <i className="fas fa-globe-americas" />
           {alum.website}
         </a>
       )}
       {alum.linkedIn && (
-        <a href={alum.linkedIn}>
+        <a href={alum.linkedIn} target="blank">
           <i className="fab fa-linkedin-in" />
           LinkedIn Profile
         </a>
       )}
       {alum.email && (
-        <a href={`mailto:${alum.email}`}>
+        <a href={`mailto:${alum.email}`} target="blank">
           <i className="fas fa-envelope" />
           {alum.email}
         </a>
