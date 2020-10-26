5a31bc9bda28a7c252148b2de71f43ff30f50026
jonathanchaffer <jonathanchaffer@gmail.com>
2020-10-25 20:04:36 -0400

    reorganize peopleGroveService.ts

--- src/services/peopleGroveService.ts
+++ src/services/peopleGroveService.ts
@@ -1,26 +1,24 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { PeopleGroveAlum } from "models";
 import * as placeholderAlumni from "placeholders/placeholder-alumni.json";
 
 const axiosInstance = axios.create({
   baseURL: "https://cors-anywhere.herokuapp.com/https://connection.hope.edu/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export async function getPeopleGroveAlumni(): Promise<PeopleGroveAlum[]> {
-  return placeholderAlumni.users.map(user => {
-    return { ...user, type: "alum" } as PeopleGroveAlum;
-  });
-}
-
-// TODO: temporary
-export function isLoggedInToPG(): boolean {
+export function getPGUser(): any | undefined {
   const item = localStorage.getItem("pg_user");
-  if (item === null) return false;
-  const pgUser = JSON.parse(item);
-  return pgUser.found;
+  if (item === null) return undefined;
+  return JSON.parse(item);
 }
 
-// TODO: temporary
+export function isLoggedInToPG(): boolean {
+  return getPGUser()?.found || false;
+}
+
 export function loginToPG(email: string, password: string): Promise<void> {
   const data = JSON.stringify({
     email,
@@ -28,14 +26,38 @@ export function loginToPG(email: string, password: string): Promise<void> {
     password,
   });
 
-  const config: AxiosRequestConfig = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  return axiosInstance.post("auth", data, config).then(response => {
+  return axiosInstance.post("auth", data).then(response => {
     if (response.data.found) localStorage.setItem("pg_user", JSON.stringify(response.data));
     else throw new Error("Invalid credentials");
+  });
+}
+
+// TODO: get actual data from peoplegrove
+export async function getPeopleGroveAlumni(): Promise<PeopleGroveAlum[]> {
+  // const accessToken = getPGUser()?.token;
+
+  // const data = {
+  //   filters: { condition: "UNION ALL", rules: [] },
+  //   includeUserId: [],
+  //   matchIndex: { index: 0, pageId: "person" },
+  //   page: 0,
+  //   tabType: "active",
+  //   userBuckets: false,
+  //   viewType: "map",
+  // };
+
+  // const options = {
+  //   headers: {
+  //     authorization: `bearer ${accessToken}`,
+  //   },
+  // };
+
+  // return axiosInstance.post("get-network-hub-users", data, options).then(response =>
+  //   response.data.users.map((user: any) => {
+  //     return { ...user, type: "alum" } as PeopleGroveAlum;
+  //   }),
+  // );
+  return placeholderAlumni.users.map(user => {
+    return { ...user, type: "alum" } as PeopleGroveAlum;
   });
 }
