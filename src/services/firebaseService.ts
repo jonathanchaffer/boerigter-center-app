5e1683cd956aa70ec2889669a843b360bc499a6d
jonathanchaffer <jonathanchaffer@gmail.com>
2020-11-13 15:42:17 -0500

    add addAlumStory function; add docs

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,6 +1,7 @@
 import { db } from "index";
 import { CuratedAlum } from "models";
 
+/** Retrieves an array of alumni from the database as a CuratedAlum[]. */
 export async function getAlumniStories(): Promise<CuratedAlum[]> {
   const snapshot = await db.collection("alumniStories").get();
   return snapshot.docs.map(doc => {
@@ -8,11 +9,20 @@ export async function getAlumniStories(): Promise<CuratedAlum[]> {
   });
 }
 
+/** Retrieves a specific alum from the database as a CuratedAlum. */
 export async function getAlumStory(id: string): Promise<CuratedAlum> {
   const snapshot = await db.doc(`alumniStories/${id}`).get();
   return { ...snapshot.data(), id: snapshot.id } as CuratedAlum;
 }
 
+/** Updates an existing alum in the database. Some or all fields may
+ * be updated, depending on what's included in the newAlum param. */
 export async function updateAlumStory(id: string, newAlum: CuratedAlum): Promise<void> {
   return db.doc(`alumniStories/${id}`).update({ newAlum });
+}
+
+/** Adds a new alum to the database and returns the id of the created document. */
+export async function addAlumStory(newAlum: CuratedAlum): Promise<string> {
+  const snapshot = await db.collection("alumniStories").add(newAlum);
+  return snapshot.id;
 }
