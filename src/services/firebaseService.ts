f9c049c2c8e122cd357153d9bb19fedd0b4f9082
jonathanchaffer <jonathanchaffer@gmail.com>
2020-11-13 15:06:59 -0500

    rename uid to id in CuratedAlum

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -4,11 +4,11 @@ import { CuratedAlum } from "models";
 export async function getAlumniStories(): Promise<CuratedAlum[]> {
   const snapshot = await db.collection("alumniStories").get();
   return snapshot.docs.map(doc => {
-    return { ...doc.data(), uid: doc.id } as CuratedAlum;
+    return { ...doc.data(), id: doc.id } as CuratedAlum;
   });
 }
 
-export async function getAlumStory(uid: string): Promise<CuratedAlum> {
-  const snapshot = await db.doc(`alumniStories/${uid}`).get();
-  return snapshot.data() as CuratedAlum;
+export async function getAlumStory(id: string): Promise<CuratedAlum> {
+  const snapshot = await db.doc(`alumniStories/${id}`).get();
+  return { ...snapshot.data(), id: snapshot.id } as CuratedAlum;
 }
