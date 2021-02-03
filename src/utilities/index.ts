fc16bddfb4065508f6bfa0167e64f5d8e8aa1c20
jonathanchaffer <jonathanchaffer@gmail.com>
2021-02-03 08:27:06 -0500

    add enum for URL paths

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,1 +1,2 @@
 export * from "./stringUtilities";
+export * from "./urlPaths";
---
+++ src/utilities/urlPaths.ts
@@ -0,0 +1,7 @@
+export const URLPaths = {
+  alumFinder: "/alumni",
+  alumStories: "/stories",
+  careerFinder: "/careers",
+  offCampusFinder: "/study-abroad",
+  poll: "/poll",
+};
