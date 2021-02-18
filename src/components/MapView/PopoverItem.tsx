add128ae217849c271dcd825a0deeb19e2bbefeb
jonathanchaffer <jonathanchaffer@gmail.com>
2021-02-18 10:12:36 -0500

    show animation while job is loading

--- src/components/MapView/PopoverItem.tsx
+++ src/components/MapView/PopoverItem.tsx
@@ -1,7 +1,8 @@
 import genericAvatar from "assets/images/generic_avatar.jpg";
 import { Mappable, PeopleGroveAlum } from "models";
-import React, { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useCallback } from "react";
+import { useAsync } from "react-async";
+import { Col, Row, Spinner } from "react-bootstrap";
 import Img from "react-cool-img";
 import { getPeopleGroveAlum } from "services";
 import { commaSeparatedList } from "utilities";
@@ -11,65 +12,66 @@ interface PopoverItemProps<I extends Mappable> {
 }
 
 export function PopoverItem<I extends Mappable>({ item }: PopoverItemProps<I>): JSX.Element {
-  const [detailedAlum, setDetailedAlum] = useState<PeopleGroveAlum | undefined>(undefined);
-
-  // In case any additional data needs to be retrieved once the popover item shows up.
-  useEffect(() => {
-    switch (item.type) {
-      case "alum": {
-        const alum = (item as unknown) as PeopleGroveAlum;
-        getPeopleGroveAlum(alum.identifier).then(alumDetails => {
-          console.log(alumDetails);
-          setDetailedAlum(alumDetails);
-        });
-        break;
-      }
-      default: {
-        break;
-      }
-    }
-  }, [item]);
-
   switch (item.type) {
     case "alum": {
       const alum = (item as unknown) as PeopleGroveAlum;
-
-      return (
-        <Row className="alum-popover-item">
-          <Col xs={3} className="d-flex align-items-center">
-            <div className="img-circle-container">
-              <Img
-                src={alum.photoUrl}
-                placeholder={genericAvatar}
-                alt={`${alum.firstName} ${alum.lastName}`}
-                width="100%"
-                loading="lazy"
-              />
-            </div>
-          </Col>
-          <Col className="d-flex align-items-center">
-            <div>
-              <h4>{`${alum.firstName} ${alum.lastName}`}</h4>
-              <div className="secondary-info d-flex flex-column">
-                {alum.majors?.length > 0 && (
-                  <span>
-                    <i className="fas fa-graduation-cap" />
-                    {commaSeparatedList(alum.majors)}
-                  </span>
-                )}
-                {detailedAlum?.workHistory[0] && (
-                  <span>
-                    <i className="fas fa-briefcase" />
-                    {detailedAlum.workHistory[0].companyTitle}
-                  </span>
-                )}
-              </div>
-            </div>
-          </Col>
-        </Row>
-      );
+      return <AlumPopoverItem alum={alum} />;
     }
     default:
       return <span>#{item.id}</span>;
   }
+}
+
+interface AlumPopoverItemProps {
+  alum: PeopleGroveAlum;
+}
+
+function AlumPopoverItem({ alum }: AlumPopoverItemProps): JSX.Element {
+  const promiseFn = useCallback(() => getPeopleGroveAlum(alum.identifier), [alum.identifier]);
+  const { data, isPending } = useAsync({ promiseFn });
+
+  return (
+    <>
+      <Row className="alum-popover-item">
+        {isPending ? (
+          <Col className="d-flex justify-content-center">
+            <Spinner animation="border" variant="secondary" size="sm" />
+          </Col>
+        ) : (
+          <>
+            <Col xs={3} className="d-flex align-items-center">
+              <div className="img-circle-container">
+                <Img
+                  src={alum.photoUrl}
+                  placeholder={genericAvatar}
+                  alt={`${alum.firstName} ${alum.lastName}`}
+                  width="100%"
+                  lazy
+                />
+              </div>
+            </Col>
+            <Col className="d-flex align-items-center">
+              <div>
+                <h4>{`${alum.firstName} ${alum.lastName}`}</h4>
+                <div className="secondary-info d-flex flex-column">
+                  {alum.majors?.length > 0 && (
+                    <span>
+                      <i className="fas fa-graduation-cap" />
+                      {commaSeparatedList(alum.majors)}
+                    </span>
+                  )}
+                  {data?.workHistory[0] && (
+                    <span>
+                      <i className="fas fa-briefcase" />
+                      {data.workHistory[0].companyTitle}
+                    </span>
+                  )}
+                </div>
+              </div>
+            </Col>
+          </>
+        )}
+      </Row>
+    </>
+  );
 }
