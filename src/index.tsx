b87fd6c51d265e52e317a86681b3266c8b00cead
jonathanchaffer <jonathanchaffer@gmail.com>
2021-02-23 12:11:04 -0500

    print error message if api key is missing

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,12 @@ import ReactDOM from "react-dom";
 import "styles/fonts.scss";
 import "styles/index.scss";
 
+if (!process.env.REACT_APP_GOOGLE_API_KEY)
+  // eslint-disable-next-line no-console
+  console.error(
+    "Google API key not found in your local repository. Make sure that you have a .env file in your root directory with the correct contents.",
+  );
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   appId: "1:600507081671:web:acf828e427865ecb0c2fe2",
