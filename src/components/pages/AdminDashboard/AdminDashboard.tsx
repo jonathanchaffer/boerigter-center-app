6c95d8f616d7d03d0c423aafedc35f5c68230197
jonathanchaffer <jonathanchaffer@gmail.com>
2021-03-02 12:43:33 -0500

    wait for user to load before loading admin dashboard

--- src/components/pages/AdminDashboard/AdminDashboard.tsx
+++ src/components/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,15 +1,32 @@
 import { LoginModal, PageContainer } from "components";
 import { UserContext } from "contexts";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { loginAsAdmin, logout } from "services";
+import { setTimeout } from "timers";
 import { URLPaths } from "utilities";
 
 export function AdminDashboard(): JSX.Element {
   const user = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return (
+  useEffect(() => {
+    setIsLoading(true);
+    /* Adds a little bit of buffer time to check if user exists, so that if the
+     * user IS logged in, they don't see the login modal briefly when opening the
+     * admin page. */
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  return isLoading ? (
+    <PageContainer>
+      <Spinner animation="border" />
+    </PageContainer>
+  ) : (
     <>
       <LoginModal
         isLoggedIn={!!user}
