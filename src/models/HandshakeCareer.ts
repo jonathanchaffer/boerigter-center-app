741d1463ad842ad5fc4d76df0e62b605711967a9
williamvonseeger <william.vonseeger@hope.edu>
2021-03-19 07:31:16 -0400

    Updated HandshakeCareer to hold job city name and modified method to to job city name when parsing response data

--- src/models/HandshakeCareer.ts
+++ src/models/HandshakeCareer.ts
@@ -2,6 +2,7 @@ import { Mappable } from "models";
 
 export interface HandshakeCareer extends Mappable {
   type: "career";
+  job_city: string;
   job_name: string;
   employer_name: string;
   employment_type_name: string;
