55b6e100f9819342b560d8b47a57e6e61c0ddd8d
jonathanchaffer <jonathanchaffer@gmail.com>
2021-03-31 19:53:50 -0400

    fix bug when uploading photo for new alum

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -3,5 +3,8 @@ import { storageRef } from "index";
 import { CuratedAlum } from "models";
 
 export function uploadProfilePhoto(file: File, alum: CuratedAlum): firebase.storage.UploadTask {
-  return storageRef.child("profilePhotos").child(alum.id).put(file);
+  return storageRef
+    .child("profilePhotos")
+    .child(alum.id || file.name)
+    .put(file);
 }
