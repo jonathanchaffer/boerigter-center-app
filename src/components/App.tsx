aafe338b4faf03d3939acb4ff88ef1bfa03449a8
jonathanchaffer <jonathanchaffer@gmail.com>
2021-04-06 18:04:44 -0400

    add todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -24,6 +24,7 @@ export function App(): JSX.Element {
     PeopleGroveAlumniContext,
   );
 
+  // TODO: rename to handleMoveNavbarButtonClick
   function handleClick() {
     if (navPosition === "top") {
       setNavPosition("bottom");
