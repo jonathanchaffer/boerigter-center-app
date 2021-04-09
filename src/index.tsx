ed4f1e326722c12edabdc44ac54d11c68e54960e
jonathanchaffer <jonathanchaffer@gmail.com>
2021-04-08 21:49:16 -0400

    test commit with eslint errors

--- src/index.tsx
+++ src/index.tsx
@@ -31,13 +31,13 @@ if (missingEnvContents.length > 0) {
 }
 
 const firebaseConfig = {
+  storageBucket: "boerigter-center-app.appspot.com",
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   appId: "1:600507081671:web:acf828e427865ecb0c2fe2",
   authDomain: "boerigter-center-app.firebaseapp.com",
   databaseURL: "https://boerigter-center-app.firebaseio.com",
   messagingSenderId: "600507081671",
   projectId: "boerigter-center-app",
-  storageBucket: "boerigter-center-app.appspot.com",
 };
 
 firebase.initializeApp(firebaseConfig);
