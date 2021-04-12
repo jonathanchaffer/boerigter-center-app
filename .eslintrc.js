3b3768fed54677f4d268fab19e93ea2965ae547f
jonathanchaffer <jonathanchaffer@gmail.com>
2021-04-12 10:47:26 -0400

    revert change to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2020: true,
   },
   extends: [
-    "eslint-config-airbnb",
+    "airbnb",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
