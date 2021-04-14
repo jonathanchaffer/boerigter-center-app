53b68090bfc1d9cfc5018c6755659eb944c07f90
jonathanchaffer <jonathanchaffer@gmail.com>
2021-04-14 09:12:46 -0400

    add docs for peopleGroveService.ts and storageService.ts

--- src/services/storageService.ts
+++ src/services/storageService.ts
@@ -2,6 +2,13 @@ import firebase from "firebase";
 import { storageRef } from "index";
 import { CuratedAlum } from "models";
 
+/**
+ * Uploads a profile photo for an alum.
+ *
+ * @param file The file to be uploaded.
+ * @param alum The alum to which the profile photo is assigned.
+ * @returns An UploadTask that allows you to monitor and manage the upload.
+ */
 export function uploadProfilePhoto(file: File, alum: CuratedAlum): firebase.storage.UploadTask {
   return storageRef
     .child("profilePhotos")
