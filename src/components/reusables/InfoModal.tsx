5a368db29efed575538d9c017faa9359cd3d3313
jonathanchaffer <jonathanchaffer@gmail.com>
2021-04-14 14:35:46 -0400

    add docs to InfoModal.tsx

--- src/components/reusables/InfoModal.tsx
+++ src/components/reusables/InfoModal.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Button, Modal } from "react-bootstrap";
 
 interface InfoModalProps {
+  /** Whether the modal should be visible. */
   show: boolean;
+  /** Function to be called when the modal is dismissed. */
   onHide: () => void;
+  /** Title to be displayed on the modal. */
   title: string;
+  /** Message to be displayed on the modal. */
   message: string;
 }
 
+/** Reusable component for displaying general information, without action. */
 export function InfoModal({ show, onHide, title, message }: InfoModalProps): JSX.Element {
   return (
     <Modal show={show} onHide={onHide} centered>
