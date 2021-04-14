818ae5ddceabf3b8adaf8412077ff47c2aac7833
jonathanchaffer <jonathanchaffer@gmail.com>
2021-04-14 14:12:39 -0400

    update docs in firebaseService.ts

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -2,7 +2,11 @@ import { db } from "index";
 import { CuratedAlum } from "models";
 import { dbCollections } from "utilities";
 
-/** Retrieves an array of alumni from the database as a CuratedAlum[]. */
+/**
+ * Retrieves all curated alumni from the database.
+ *
+ * @returns A Promise containing the retrieved documents as CuratedAlum objects.
+ */
 export async function getAlumniStories(): Promise<CuratedAlum[]> {
   const snapshot = await db.collection(dbCollections.alumniStories).get();
   return snapshot.docs.map(doc => {
@@ -10,25 +14,46 @@ export async function getAlumniStories(): Promise<CuratedAlum[]> {
   });
 }
 
-/** Retrieves a specific alum from the database as a CuratedAlum. */
+/**
+ * Retrieves a specific alum from the database as a CuratedAlum.
+ *
+ * @param id The ID of the alum to retrieve.
+ * @returns A Promise containing a specific alum, or undefined if the ID doesn't exist.
+ */
 export async function getAlumStory(id: string): Promise<CuratedAlum | undefined> {
   const snapshot = await db.doc(`${dbCollections.alumniStories}/${id}`).get();
   return snapshot.data() ? ({ ...snapshot.data(), id: snapshot.id } as CuratedAlum) : undefined;
 }
 
-/** Updates an existing alum in the database. Some or all fields may
- * be updated, depending on what's included in the newAlum param. */
+/**
+ * Updates an existing alum in the database. Some or all fields may
+ * be updated, depending on what's included in the newAlum param.
+ *
+ * @param id The ID of the alum to update.
+ * @param newAlum The updated version of the alum.
+ * @returns A Promise representing the status of the update.
+ */
 export async function updateAlumStory(id: string, newAlum: CuratedAlum): Promise<void> {
   return db.doc(`${dbCollections.alumniStories}/${id}`).update({ ...newAlum });
 }
 
-/** Adds a new alum to the database and returns the id of the created document. */
+/**
+ * Adds a new alum to the database and returns the id of the created document.
+ *
+ * @param newAlum The alum to add.
+ * @returns A Promise containing the ID of the added alum.
+ */
 export async function addAlumStory(newAlum: CuratedAlum): Promise<string> {
   const snapshot = await db.collection(dbCollections.alumniStories).add(newAlum);
   return snapshot.id;
 }
 
-/** Deletes an alum from the database. */
+/**
+ * Deletes an alum from the database.
+ *
+ * @param id The ID of the alum to delete.
+ * @returns A Promise representing the status of the delete.
+ */
 export async function deleteAlumStory(id: string): Promise<void> {
   return db.doc(`${dbCollections.alumniStories}/${id}`).delete();
 }
